2c809a0c7946896d39c2edb03b2ad95b1ad39a48
frankbydefault <franco.hauva@gmail.com>
2021-06-10 12:57:33 -0400

    conexion a firebase

--- assets/db-connect.js
+++ assets/db-connect.js
@@ -1,1 +1,16 @@
-//hola
+import firebase from "firebase/app";
+import "firebase/database";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://tic-proy2-default-rtdb.firebaseio.com/",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+firebase.initializeApp(firebaseConfig);
