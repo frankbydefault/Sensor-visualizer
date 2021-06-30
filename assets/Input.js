dfcf01b09fdd7cda0aeb99ed582874cc3ff47190
dvher <diego.vergara5@mail.udp.cl>
2021-06-30 16:23:51 -0400

    Cambios input.js

--- assets/Input.js
+++ assets/Input.js
@@ -4,15 +4,25 @@ import { SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
 const Input = () => {
   const [number, setNumber] = React.useState(null);
 
+  const isNumeric = (value) => {
+
+    if(!isNaN(+value)){
+  
+      setNumber(value <= 80 && value > 0 ? value : 10)
+  
+    }else setNumber(0)
+  
+  }
+
   return (
     <SafeAreaView>
       <TextInput
         style={styles.input}
-        onChangeText={(value) => setNumber(value)}
+        onChangeText={(value) => isNumeric(value)}
         keyboardType="numeric"
       />
 
-      <Text>numero: {number}</Text>
+      <Text style={styles.text}>Numero: {number}</Text>
     </SafeAreaView>
   );
 };
@@ -22,9 +32,19 @@ const styles = StyleSheet.create({
     height: 40,
     width: 40,
     borderWidth: 1,
-    borderRadius: 10,
-    float: "center",
+    borderRadius: 8,
+    padding: 12,
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
+  text: {
+
+    overflow: "hidden",
+    position: "relative",
+    width: "100%",
+    alignItems: "center"
+
+  }
 });
 
 export default Input;
